084eb9d1d6f2d1dc5f06cf96c6f3ca092ee6c584
account@cacoveanu.com <account@cacoveanu.com>
2022-10-02 06:00:50 +0000

    working unzip in node js

--- zipTest.js
+++ zipTest.js
@@ -10,44 +10,47 @@
 } from "static/libs/zip.min.js"*/
 
 (async function main () {
-
     var fs = require('fs')
-    var zip = require('./static/libs/zip.min.js')
+    const stream = require('stream')
+    //var zip = require('./static/libs/zip.js')
+    var jszip = require('./static/libs/jszip.js')
 
     let filePath = './static/books/Feature_Comics_111.cbz'
-    /*var data = fs.readFile(filePath, function(err, data) {
-        if (err) {
-            return null
-        }
-        return data
-    })*/
-
     let data = fs.readFileSync(filePath)//.toString('hex');
-    console.log(data)
-    let blob = new Blob([data])
-    console.log(blob)
 
-    const zipFileReader = new zip.BlobReader(blob)
-    const zipReader = new zip.ZipReader(zipFileReader)
+    var zip = new jszip()
 
-    /*async function getEntries(zipReader) {
-        try {
-            let entries = await zipReader.getEntries()
-            return entries
-        } catch (error) {
-            console.log("error: " + error)
-        }
-    }*/
+    var z = await zip.loadAsync(data)
+    //console.log(z.files)
+    //console.log(typeof(z.files))
+    console.log(Object.entries(z.files)[1])
 
-    //zipReader.getEntries().then((entries) => console.log(entries))
-    /*let entries = await getEntries(zipReader)*/
-    let entries = await zipReader.getEntries()
-    console.log(entries)
+    var filename = Object.entries(z.files)[1][0]
+    console.log(filename)
+    console.log(z.files[filename])
 
-    /*const firstEntry = (await zipReader.getEntries()).shift()
-    const helloWorldText = await firstEntry.getData(helloWorldWriter)
-    await zipReader.close()
+    // https://stuk.github.io/jszip/documentation/api_zipobject/async.html
+    var image = await z.files[filename].async("base64")
+    console.log(image)
 
-    console.log(helloWorldText)*/
+    var imagearr = await z.files[filename].async("uint8array")
+    fs.writeFileSync("page.jpg", imagearr)
+
+    //let blob = new Blob([data])
+
+    //const zipFileReader = new zip.BlobReader(blob)
+    //const zipReader = new zip.ZipReader(zipFileReader)
+    //let entries = await zipReader.getEntries()
+    //console.log(entries)
+
+    //const firstEntry = entries[1]
+    //console.log(firstEntry)
+    //const imageFileWriter = new zip.BlobWriter()
+    //let bytes = await firstEntry.getData(imageFileWriter)
+    //await zipReader.close()
+
+    //console.log(bytes)
+
+    //fs.writeFileSync("page.jpg", bytes)
 
 })()
